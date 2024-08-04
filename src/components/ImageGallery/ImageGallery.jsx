c565b5ca95615e8e9038a5f86a3e177368886e7c
Spajkowiecki <pcio10@wp.pl>
2024-08-04 17:03:55 +0200

    ImageGallery to Function
    Change ImageGallery class to function

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,32 +3,28 @@ import PropTypes from 'prop-types';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import style from './ImageGallery.module.css';
 
-class ImageGallery extends Component {
-  getClickedImage = value => {
-    this.props.selectedImage(value);
-  };
-  render() {
-    const { images } = this.props;
-    return (
-      <ul className={style.gallery}>
-        {images.map(image => {
-          return (
-            <ImageGalleryItem
-              key={image.id}
-              image={image}
-              clickedImage={values => this.getClickedImage(values)}
-            />
-          );
-        })}
-      </ul>
-    );
+export default function ImageGallery({ images, selectedImage }) {
+  function getClickedImage(value) {
+    selectedImage(value);
   }
+
+  return (
+    <ul className={style.gallery}>
+      {images.map(image => {
+        return (
+          <ImageGalleryItem
+            key={image.id}
+            image={image}
+            clickedImage={values => getClickedImage(values)}
+          />
+        );
+      })}
+    </ul>
+  );
 }
 
 ImageGallery.propTypes = {
   images: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
-
-export default ImageGallery;
 
 //! THIS COMPONENT SHOULD RERENDER WHEN GALLERY CHANGE
