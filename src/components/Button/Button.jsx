250ef939f462d1e81a72b39dd8cf26860902b2e3
Spajkowiecki <pcio10@wp.pl>
2024-08-04 16:10:44 +0200

    Button component update
    Changed class component to function component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,15 +2,10 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import style from './Button.module.css';
 
-class Button extends Component {
-  render() {
-    const { nextPage } = this.props;
-    return (
-      <button onClick={nextPage} className={style.button}>
-        {'+'}
-      </button>
-    );
-  }
+export default function Button({ nextPage }) {
+  return (
+    <button onClick={nextPage} className={style.button}>
+      {'+'}
+    </button>
+  );
 }
-
-export default Button;
